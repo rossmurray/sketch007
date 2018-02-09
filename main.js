81fe09353d115f5f01b3482aa9086768f997a4b3
rossmurray <rm@egoorb.com>
2018-02-09 01:57:42 -0600

    make colored light chasing prototype

--- main.js
+++ main.js
@@ -8,46 +8,13 @@ var fnMain = (function() {
     }
 
     function getConfig() {
-        const patternRepeats = 3;
-        const twillWidth = 4;
-        const mergePatternEnds = false;
-        const showWarp = true;
-        const showWeft = true;
-        const enableMask = true;
-        const stripeBlendMode = PIXI.BLEND_MODES.NORMAL;
-        const backgroundColor = colorStringToNumber('black');
-        const screenMargin = 0;
-        const cyclePause = 0;
-        const randomSequenceStrategy = {
-            stripesMin: 4,
-            stripesMax: 17,
-            widthMin: 1,
-            widthMax: 20,
-        }
-        const markovSequenceStrategy = {
-            stripesMin: 6,
-            stripesMax: 24,
-        };
-        const randomColorStrategy = {
-            maxBaseColors: 5,
-        }
-        const strategies = {
-            randomColor: randomColorStrategy,
-            randomSequence: randomSequenceStrategy,
-            markovSequence: markovSequenceStrategy,
-        }
+        const backgroundColor = colorStringToNumber('#eeeeee');
+        const buttonPadding = 0.075; //total space allocated to padding, not per-button.
+        const screenMargin = 0.02;
         const config = {
-            stripeBlendMode: stripeBlendMode,
-            patternRepeats: patternRepeats,
-            cyclePause: cyclePause,
             screenMargin: screenMargin,
             backgroundColor: backgroundColor,
-            twillWidth: twillWidth,
-            mergePatternEnds: mergePatternEnds,
-            showWarp: showWarp,
-            showWeft: showWeft,
-            enableMask: enableMask,
-            strategies: strategies,
+            buttonPadding: buttonPadding,
         };
         return config;
     }
@@ -55,9 +22,12 @@ var fnMain = (function() {
     function makeBoardRectangle(margin, viewRectangle) {
         const xmargin = margin * viewRectangle.width;
         const ymargin = margin * viewRectangle.height;
-        const boardWidth = viewRectangle.width - (xmargin * 2);
-        const boardHeight = viewRectangle.height - (ymargin * 2);
-        return new PIXI.Rectangle(xmargin, ymargin, boardWidth, boardHeight);
+        const finalMargin = Math.floor(xmargin + ymargin);
+        const boardWidth = viewRectangle.width - finalMargin;
+        const boardHeight = viewRectangle.height - finalMargin;
+        const x = Math.floor((viewRectangle.width - boardWidth) / 2);
+        const y = Math.floor((viewRectangle.height - boardHeight) / 2);
+        return new PIXI.Rectangle(x, y, boardWidth, boardHeight);
     }
 
     function makeRange(n) {
@@ -75,6 +45,14 @@ var fnMain = (function() {
 
     function colorNumberToHexString(number) {
         return chroma(number).hex();
+    }
+
+    function chromaToNumber(chrom) {
+        const rgbArray = chrom.rgb();
+        let result = Math.floor(rgbArray[2])
+            | Math.floor(rgbArray[1]) << 8
+            | Math.floor(rgbArray[0]) << 16;
+        return result;
     }
 
     function portion(i, size) {
@@ -99,6 +77,36 @@ var fnMain = (function() {
         return values[values.length - 1];
     }
 
+    function makeKeyHandler(keyCode) {
+        const state = {
+            code: keyCode,
+            isPressed: false,
+            onDown: undefined,
+            onUp: undefined,
+        };
+        function keyUp(event, state) {
+            if(event.keyCode == state.code) {
+                if(state.isPressed && state.onUp) {
+                    state.onUp();
+                }
+                state.isPressed = false;
+                event.preventDefault();
+            }
+        }
+        function keyDown(event, state) {
+            if(event.keyCode == state.code) {
+                if(!state.isPressed && state.onDown) {
+                    state.onDown();
+                }
+                state.isPressed = true;
+                event.preventDefault();
+            }
+        }
+        window.addEventListener('keyup', e => keyUp(e, state), false);
+        window.addEventListener('keydown', e => keyDown(e, state), false);
+        return state;
+    }
+
     function makeBackground(config, screenRect, renderer) {
         const canvasElement = document.createElement('canvas');
         canvasElement.width = screenRect.width;
@@ -111,7 +119,7 @@ var fnMain = (function() {
         return sprite;
     }
 
-    function mapSprite(obj) {
+    function mapSprite(obj, renderer) {
         const width = obj.width;
         const height = obj.height;
         const gfx = new PIXI.Graphics();
@@ -126,33 +134,101 @@ var fnMain = (function() {
         const sprite = new PIXI.Sprite(texture);
         sprite.x = obj.x;
         sprite.y = obj.y;
-        sprite.blendMode = config.stripeBlendMode;
         return sprite;
     }
 
-    function animateShapes(shapes, board, config) {
-        const timeline = anime.timeline({
-            autoplay: false,
-            loop: true,
-            duration: 0,
-        });
-        const dummy = {x:0};
-        for(let i = 0; i < shapes.length; i++) {
-            const shape = shapes[i];
-            // timeline.add({
-            //     targets: shape.sprite.scale,
-            //     x: shrinkAnimation,
-            //     y: shrinkAnimation,
-            //     easing: config.shrinkEasing,
-            //     offset: offset,
-            // });
+    function createGame(gameConfig) {
+        const gameState = {
+            playerMoveMode: 0,
+            modSetting: 3,
+            moveCount: 0,
+            boards: [],
+        };
+        const board1 = makeRange(25).map(x => 0);
+        const board2 = makeRange(25).map(x => 0);
+        const board3 = makeRange(25).map(x => 0);
+        const boards = [board1, board2, board3];
+        gameState.boards = boards;
+        function makeNeighborList(centerNode) {
+            if(!(centerNode >= 0 && centerNode < 25) || centerNode != Math.floor(centerNode)) {
+                throw "invalid centerNode value: [" + centerNode + "]";
+            }
+            const result = [centerNode];
+            if(centerNode % 5 != 0) result.push(centerNode-1);
+            if(centerNode % 5 != 4) result.push(centerNode+1);
+            if(centerNode > 4) result.push(centerNode-5);
+            if(centerNode < 20) result.push(centerNode+5);
+            return result;
         }
-        // timeline.add({
-        //     targets: dummy,
-        //     x: 0,
-        //     duration: config.cyclePause,
-        // });
-        return timeline;
+        const neighborsTable = makeRange(25).map(x => makeNeighborList(x));
+        for(let i = 0; i < 17; i++) {
+            const button = Math.floor(Math.random() * 25);
+            const boardChoice = Math.floor(Math.random() * 3);
+            pressButton(button, boards[boardChoice], neighborsTable, gameState);
+        }
+        function boardsToColors(boards, modSetting) {
+            const toChannel = x => x / (modSetting - 1) * 255;
+            const colors = boards[0].map((x,i) => {
+                const r = toChannel(x);
+                const g = toChannel(boards[1][i]);
+                const b = toChannel(boards[2][i]);
+                return chromaToNumber(chroma(r,g,b));
+            });
+            return colors;
+        }
+        function pressButton(buttonNumber, board, neighborsTable, gameState) {
+            const neighbors = neighborsTable[buttonNumber];
+            for(let neighbor of neighbors) {
+                board[neighbor] = (board[neighbor] + 1) % gameState.modSetting;
+            }
+            gameState.moveCount += 1;
+        }
+        function increaseMoveMode(amount) {
+            gameState.playerMoveMode += amount;
+            gameState.playerMoveMode = gameState.playerMoveMode % 3;
+        }
+        gameState.getBoardColors = () => boardsToColors(boards, gameState.modSetting);
+        gameState.pressButton = buttonNumber => pressButton(buttonNumber, boards[gameState.playerMoveMode], neighborsTable, gameState);
+        gameState.increaseMoveMode = increaseMoveMode;
+        return gameState;
+    }
+
+    function makeButtons(config, boardRect, renderer, game) {
+        const sprites = makeRange(25).map(i => {
+            const xIndex = i % 5;
+            const yIndex = Math.floor(i / 5);
+            const minDimension = boardRect.width < boardRect.height ? boardRect.width : boardRect.height;
+            const maxDimension = boardRect.width > boardRect.height ? boardRect.width : boardRect.height;
+            const totalPadding = config.buttonPadding * minDimension;
+            const paddingBetweenButtons = Math.floor(totalPadding / 4); //none on the outside.
+            const buttonWidth = Math.floor((minDimension - totalPadding) / 5);
+            const actualUsedSpace = buttonWidth * 5 + paddingBetweenButtons * 4;
+            const xMargin = Math.floor((boardRect.width - actualUsedSpace) / 2);
+            const yMargin = Math.floor((boardRect.height - actualUsedSpace) / 2);
+            const obj = {
+                width: buttonWidth,
+                height: buttonWidth,
+                color: 0xFFFFFF,
+                x: boardRect.x + xMargin + xIndex * buttonWidth + xIndex * paddingBetweenButtons,
+                y: boardRect.y + yMargin + yIndex * buttonWidth + yIndex * paddingBetweenButtons,
+            };
+            const sprite = mapSprite(obj, renderer);
+            sprite.tint = 0xFFFFFF;
+            sprite.interactive = true;
+            sprite.buttonMode = true;
+            return sprite;
+        });
+        return sprites;
+    }
+
+    function buttonClicked(buttonIndex, sprites, game) {
+        game.pressButton(buttonIndex);
+        const newColors = game.getBoardColors();
+        for(let i = 0; i < newColors.length; i++) {
+            if(sprites[i]) {
+                sprites[i].tint = newColors[i];
+            }
+        }
     }
 
     return (function() {
@@ -172,21 +248,30 @@ var fnMain = (function() {
         app.ticker.autoStart = false;
         app.ticker.stop();
 
-        let board = makeBoardRectangle(config.screenMargin, app.screen);
-        const shapes = [];//makePattern(config, board, app.renderer);
+        let boardRect = makeBoardRectangle(config.screenMargin, app.screen);
+        const game = createGame();
+        const buttons = makeButtons(config, boardRect, app.renderer, game);
+        const currentColors = game.getBoardColors();
         const background = makeBackground(config, app.screen, app.renderer);
         //app.stage.addChild(background);
-        for(let s of shapes) {
-            app.stage.addChild(s.sprite);
+        for(let i = 0; i < buttons.length; i++) {
+            const b = buttons[i];
+            b.on('pointerdown', () => buttonClicked(i, buttons, game));
+            b.tint = currentColors[i];
+            app.stage.addChild(b);
         }
-        //const animation = animateShapes(shapes, board, config);
+        const rightKey = makeKeyHandler(39);
+        const leftKey = makeKeyHandler(37);
+        rightKey.onUp = () => game.increaseMoveMode(1);
+        leftKey.onUp = () => game.increaseMoveMode(2);
         const animation = {};
         let state = {
             config: config,
             app: app,
-            board: board,
+            boardRect: boardRect,
             animation: animation,
-            shapes: shapes,
+            game: game,
+            buttons: buttons,
             background: background,
         };
         return function(recorder) {
